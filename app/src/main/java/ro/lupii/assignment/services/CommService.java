315bed6df391e541d2bbc4b3dc9e544d9ace1840
Andrei Datcu <datcuandrei@gmail.com>
2016-01-10 17:25:41 +0200

    Added SocketThread implementation

--- app/src/main/java/ro/lupii/assignment/services/CommService.java
+++ app/src/main/java/ro/lupii/assignment/services/CommService.java
@@ -2,15 +2,36 @@ package ro.lupii.assignment.services;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
 public class CommService extends Service {
-    public CommService() {
+    // Binder given to clients
+    private final IBinder mBinder = new LocalBinder();
+
+    /**
+     * Class used for the client Binder.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with IPC.
+     */
+    public class LocalBinder extends Binder {
+        CommService getService() {
+            // Return this instance of LocalService so clients can call public methods
+            return CommService.this;
+        }
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    @Override
+    public void onCreate() {
+
     }
 }
--- app/src/main/java/ro/lupii/assignment/services/SocketThread.java
+++ app/src/main/java/ro/lupii/assignment/services/SocketThread.java
@@ -1,7 +1,106 @@
 package ro.lupii.assignment.services;
 
+import android.os.Handler;
+import android.os.Looper;
+
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import ro.lupii.assignment.data.OSPSocket;
+
 /**
  * Created by andrei on 1/10/16.
  */
-public class SocketThread {
+public class SocketThread extends Thread {
+
+    public interface OnMessageArrivedListener {
+        void onMessageArrived(String message);
+    }
+
+    private class MessageWaitLock {
+        public boolean somebodyWaiting = false;
+        public String theActualMessage;
+    }
+
+    private OSPSocket socket;
+    final private ConcurrentLinkedQueue<String> inputQueue = new ConcurrentLinkedQueue<>();
+    final private MessageWaitLock mMessageWaitLock = new MessageWaitLock();
+    private OnMessageArrivedListener mOnMessageArrivedListener;
+    private Exception raisedException = null;
+
+    public SocketThread(OnMessageArrivedListener mOnMessageArrivedListener) {
+        this.mOnMessageArrivedListener = mOnMessageArrivedListener;
+    }
+
+    private void createSocket() throws IOException{
+        //TODO(John) Create socket here
+    }
+
+    @Override
+    public void run() {
+        try {
+            createSocket();
+        } catch (IOException e) {
+            raisedException = e;
+            return;
+        }
+        try {
+            for (; ; ) {
+                try {
+                    final String message = socket.readAll();
+                    synchronized (mMessageWaitLock) {
+                        if (mMessageWaitLock.somebodyWaiting) {
+                            mMessageWaitLock.theActualMessage = message;
+                            mMessageWaitLock.somebodyWaiting = false;
+                            mMessageWaitLock.notify();
+                        } else {
+                            new Handler(Looper.getMainLooper()).post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mOnMessageArrivedListener.onMessageArrived(message);
+                                }
+                            });
+                        }
+                    }
+                } catch (SocketTimeoutException e) {
+                    if (isInterrupted())
+                        return;
+                    String sendMsg = inputQueue.poll();
+                    if (sendMsg != null)
+                        socket.writeString(sendMsg);
+                } catch (IOException e) {
+                    raisedException = e;
+                    return;
+                }
+            }
+        } finally {
+            //TODO (John) close socket here;
+        }
+    }
+
+    /**
+     * Block and wait for a message to arrive
+     * WARNING!!! This HAS to be called outside this thread's context
+     * @return Received message
+     * @throws Exception
+     */
+    public String readMessage() throws Exception {
+        if (raisedException != null)
+            throw raisedException;
+        synchronized (mMessageWaitLock) {
+            mMessageWaitLock.somebodyWaiting = true;
+            mMessageWaitLock.wait();
+            return mMessageWaitLock.theActualMessage;
+        }
+    }
+
+    /**
+     * Send a new message
+     * WARNING!!! This MUST be called outside this thread's context
+     * @param message The message to be sent
+     */
+    public void sendMessage(String message) {
+        inputQueue.add(message);
+    }
 }
